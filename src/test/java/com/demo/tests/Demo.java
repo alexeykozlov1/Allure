7f2027229969af7c013144e3fd315785c90187fd
akozlov <akozlov@k12.com>
2022-12-07 11:40:11 -0500

    Demo 4

--- src/test/java/com/demo/tests/Demo.java
+++ src/test/java/com/demo/tests/Demo.java
@@ -126,48 +126,48 @@ public class Demo {
         }
     }
 
-//    @Test
-//    public void randomTest1() {
-//// create instance of Random class
-//        Random rand = new Random();
-//
-//        // Generate random integers in range 0 to 999
-//        int rand_int1 = rand.nextInt(1000);
-//        int rand_int2 = rand.nextInt(1000);
-//
-//        // Print random integers
-//        System.out.println("Random Integers: " + rand_int1);
-//        System.out.println("Random Integers: " + rand_int2);
-//        Assert.assertFalse(rand_int1 > rand_int2);
-//    }
-//
-//    @Test
-//    public void randomTest2() {
-//// create instance of Random class
-//        Random rand = new Random();
-//
-//        // Generate random integers in range 0 to 999
-//        int rand_int1 = rand.nextInt(1000);
-//        int rand_int2 = rand.nextInt(1000);
-//
-//        // Print random integers
-//        System.out.println("Random Integers: " + rand_int1);
-//        System.out.println("Random Integers: " + rand_int2);
-//        Assert.assertFalse(rand_int1 > rand_int2);
-//    }
-//
-//    @Test
-//    public void randomTest3() {
-//// create instance of Random class
-//        Random rand = new Random();
-//
-//        // Generate random integers in range 0 to 999
-//        int rand_int1 = rand.nextInt(1000);
-//        int rand_int2 = rand.nextInt(1000);
-//
-//        // Print random integers
-//        System.out.println("Random Integers: " + rand_int1);
-//        System.out.println("Random Integers: " + rand_int2);
-//        Assert.assertFalse(rand_int1 > rand_int2);
-//    }
+    @Test
+    public void randomTest1() {
+// create instance of Random class
+        Random rand = new Random();
+
+        // Generate random integers in range 0 to 999
+        int rand_int1 = rand.nextInt(1000);
+        int rand_int2 = rand.nextInt(1000);
+
+        // Print random integers
+        System.out.println("Random Integers: " + rand_int1);
+        System.out.println("Random Integers: " + rand_int2);
+        Assert.assertFalse(rand_int1 > rand_int2);
+    }
+
+    @Test
+    public void randomTest2() {
+// create instance of Random class
+        Random rand = new Random();
+
+        // Generate random integers in range 0 to 999
+        int rand_int1 = rand.nextInt(1000);
+        int rand_int2 = rand.nextInt(1000);
+
+        // Print random integers
+        System.out.println("Random Integers: " + rand_int1);
+        System.out.println("Random Integers: " + rand_int2);
+        Assert.assertFalse(rand_int1 > rand_int2);
+    }
+
+    @Test
+    public void randomTest3() {
+// create instance of Random class
+        Random rand = new Random();
+
+        // Generate random integers in range 0 to 999
+        int rand_int1 = rand.nextInt(1000);
+        int rand_int2 = rand.nextInt(1000);
+
+        // Print random integers
+        System.out.println("Random Integers: " + rand_int1);
+        System.out.println("Random Integers: " + rand_int2);
+        Assert.assertFalse(rand_int1 > rand_int2);
+    }
 }
